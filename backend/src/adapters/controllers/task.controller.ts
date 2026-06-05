d898faa717347c591137e9b9d38dd302bf34ec72
Oscar Jara <ojara@wherex.com>
2026-06-04 22:57:22 -0400

    [FIX][backend-manager-task]: import

--- backend/src/adapters/controllers/task.controller.ts
+++ backend/src/adapters/controllers/task.controller.ts
@@ -1,9 +1,9 @@
 import type { NextFunction, Request, Response } from "express";
-import type { CreateTaskUseCase } from "../../../application/use-cases/create-task.use-case.js";
-import type { DeleteTaskUseCase } from "../../../application/use-cases/delete-task.use-case.js";
-import type { GetTaskByIdUseCase } from "../../../application/use-cases/get-task-by-id.use-case.js";
-import type { ListTasksUseCase } from "../../../application/use-cases/list-tasks.use-case.js";
-import type { UpdateTaskUseCase } from "../../../application/use-cases/update-task.use-case.js";
+import type { CreateTaskUseCase } from "../../app/use-cases/create-task.use-case.js";
+import type { DeleteTaskUseCase } from "../../app/use-cases/delete-task.use-case.js";
+import type { GetTaskByIdUseCase } from "../../app/use-cases/get-task-by-id.use-case.js";
+import type { ListTasksUseCase } from "../../app/use-cases/list-tasks.use-case.js";
+import type { UpdateTaskUseCase } from "../../app/use-cases/update-task.use-case.js";
 import { mapQueryToTaskFilters } from "../mappers/task-filters.mapper.js";
 
 type Dependencies = {
@@ -22,7 +22,7 @@ export class TaskController {
       const tasks = this.deps.listTasksUseCase.execute(
         mapQueryToTaskFilters({
           completed: req.query.completed,
-          search: req.query.search
+          search: req.query.search,
         })
       );
 
@@ -53,9 +53,16 @@ export class TaskController {
 
   update = (req: Request, res: Response, next: NextFunction): void => {
     try {
-      const title = typeof req.body?.title === "string" ? req.body.title : undefined;
-      const completed = typeof req.body?.completed === "boolean" ? req.body.completed : undefined;
-      const task = this.deps.updateTaskUseCase.execute(Number(req.params.id), { title, completed });
+      const title =
+        typeof req.body?.title === "string" ? req.body.title : undefined;
+      const completed =
+        typeof req.body?.completed === "boolean"
+          ? req.body.completed
+          : undefined;
+      const task = this.deps.updateTaskUseCase.execute(Number(req.params.id), {
+        title,
+        completed,
+      });
       res.status(200).json(task);
     } catch (error) {
       next(error);
@@ -73,7 +80,9 @@ export class TaskController {
 
   complete = (req: Request, res: Response, next: NextFunction): void => {
     try {
-      const task = this.deps.updateTaskUseCase.execute(Number(req.params.id), { completed: true });
+      const task = this.deps.updateTaskUseCase.execute(Number(req.params.id), {
+        completed: true,
+      });
       res.status(200).json(task);
     } catch (error) {
       next(error);
